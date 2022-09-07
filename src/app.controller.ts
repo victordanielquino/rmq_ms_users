b96df2fccf222209bfe149d4f29ee73a7ec4849c
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-09-07 10:18:18 -0400

    add: rabbitmq ok

---
+++ src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @EventPattern('findAll')
+  async findAll(data: string) {
+    console.log(data);
+    // return this.appService.getHello();
+  }
+
+  @EventPattern('findOne')
+  async findOne(data: string) {
+    console.log(data);
+    // return this.appService.getHello();
+  }
+
+  @MessagePattern('createOne')
+  async createOne(@Payload() userDTO: any) {
+    console.log('microservice: user: ', userDTO);
+    return userDTO;
+  }
+
+  @EventPattern('updateOne')
+  async updateOne(data: string) {
+    console.log(data);
+    // return this.appService.getHello();
+  }
+
+  @EventPattern('deleteOne')
+  async deleteOne(data: string) {
+    console.log(data);
+    // return this.appService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://root:pass@localhost:5672'],
+        queue: 'main_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
